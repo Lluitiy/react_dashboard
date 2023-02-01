e358e09ec59adcd879e8099c0e1577fed9383202
Pavlo Vovzhynskyy <pavlovovzhynskyy@MacBook-Air-Pavlo.local>
2023-02-01 16:58:42 +0100

    add all pages

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,5 +1,94 @@
-const index = () => {
-	return <div>index</div>;
+import { Box, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+
+import Accordion from "@mui/material/Accordion";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+
+import Header from "../../components/Header";
+
+const FAQ = () => {
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+
+	return (
+		<Box m="20px">
+			<Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+
+			<Accordion defaultExpanded>
+				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+					<Typography color={colors.greenAccent[500]} variant="h5">
+						An Important Question
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Suspendisse malesuada lacus ex, sit amet blandit leo
+						lobortis eget.
+					</Typography>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion defaultExpanded>
+				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+					<Typography color={colors.greenAccent[500]} variant="h5">
+						Another Important Question
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Suspendisse malesuada lacus ex, sit amet blandit leo
+						lobortis eget.
+					</Typography>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion defaultExpanded>
+				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+					<Typography color={colors.greenAccent[500]} variant="h5">
+						Your Favorite Question
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Suspendisse malesuada lacus ex, sit amet blandit leo
+						lobortis eget.
+					</Typography>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion defaultExpanded>
+				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+					<Typography color={colors.greenAccent[500]} variant="h5">
+						Some Random Question
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Suspendisse malesuada lacus ex, sit amet blandit leo
+						lobortis eget.
+					</Typography>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion defaultExpanded>
+				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+					<Typography color={colors.greenAccent[500]} variant="h5">
+						The Final Question
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Suspendisse malesuada lacus ex, sit amet blandit leo
+						lobortis eget.
+					</Typography>
+				</AccordionDetails>
+			</Accordion>
+		</Box>
+	);
 };
 
-export default index;
+export default FAQ;
